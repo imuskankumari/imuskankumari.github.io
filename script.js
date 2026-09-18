895b422a657cf31b56138fd99ff88703526bf6a5
Muskan Kumari <muskankumari33300@gmail.com>
2026-09-18 14:31:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const slides = document.querySelectorAll('.hero-slide');
     const dots = document.querySelectorAll('.dot');
     let currentSlide = 0;
-    const slideIntervalTime = 6000; // 6 Seconds
+    const slideIntervalTime = 6000;
 
     function goToSlide(index) {
         slides.forEach(s => s.classList.remove('active-slide'));
